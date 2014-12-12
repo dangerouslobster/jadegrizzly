6f8844ebeef704ea6587797503a4b18d5b830ac6
Brett Leibowitz <LEIBO@Bretts-MacBook-Pro.local>
2014-12-12 12:01:04 -0800

    cleaned up console and files

--- client/contestView/contest.js
+++ client/contestView/contest.js
@@ -36,64 +36,70 @@ Template.contestPhotos.events({
   }
 });
 
-var hasUpVoted = function(voterId, photoId) {
-  var query = {'_id':photoId, 'upVotes': { $in: [ voterId ] } };
-  var voteCheck = Images.findOne(query);
-  return (voteCheck !== undefined);
-};
 
-var hasDownVoted = function(voterId, photoId) {
-  var query = {'_id':photoId, 'downVotes': { $in: [ voterId ] } };
-  var voteCheck = Images.findOne(query);
-  return (voteCheck !== undefined);
-};
-
-var setTopVote = function() {
-  var cursor1 =  Images.find({featName: Session.get('currentFeatName')});
-  var cursorArr = cursor1.fetch();
-  console.log(cursorArr);
-  var max = Number.NEGATIVE_INFINITY;
-  var maxId;
-  for (var i = 0; i < cursorArr.length; i++) {
-    if (cursorArr[i].voteCount > max) {
-      max = cursorArr[i].voteCount;
-      maxId = cursorArr[i]._id;
-    }
-  }
-
-  cursor1.forEach(function(doc, index, cursor) {
-    if (doc._id === maxId) {
-      Images.update({_id: doc._id},{$set:{'topVoted': true}});
-    } else {
-      Images.update({_id: doc._id},{$set:{'topVoted': false}});
-    }
-  });
-};
 
 /**
  * Helpers for each Photo
  */
 
+
+
+// var hasUpVoted = function(voterId, photoId) {
+//   var query = {'_id':photoId, 'upVotes': { $in: [ voterId ] } };
+//   var voteCheck = Images.findOne(query);
+//   return (voteCheck !== undefined);
+// };
+
+// var hasDownVoted = function(voterId, photoId) {
+//   var query = {'_id':photoId, 'downVotes': { $in: [ voterId ] } };
+//   var voteCheck = Images.findOne(query);
+//   return (voteCheck !== undefined);
+// };
+
+// var setTopVote = function() {
+//   var cursor1 =  Images.find({featName: Session.get('currentFeatName')});
+//   var cursorArr = cursor1.fetch();
+//   console.log(cursorArr);
+//   var max = Number.NEGATIVE_INFINITY;
+//   var maxId;
+//   for (var i = 0; i < cursorArr.length; i++) {
+//     if (cursorArr[i].voteCount > max) {
+//       max = cursorArr[i].voteCount;
+//       maxId = cursorArr[i]._id;
+//     }
+//   }
+
+//   cursor1.forEach(function(doc, index, cursor) {
+//     if (doc._id === maxId) {
+//       Images.update({_id: doc._id},{$set:{'topVoted': true}});
+//     } else {
+//       Images.update({_id: doc._id},{$set:{'topVoted': false}});
+//     }
+//   });
+// };
+
+
+Template.snapshots.helpers({
+  isOwner: function() {
+    console.log(Meteor.userId() === this.userId);
+    return Meteor.userId() === this.userId;
+  }
+});
+
 Template.snapshots.events({
   'click div.upvote': function(evt, template) {
     var userId = Meteor.userId();
-    // console.log(this);
-    var upVoteCheck = hasUpVoted(userId, this._id);
+
+    var upVoteCheck = hasUpVoted( userId, this._id);
     var downVoteCheck = hasDownVoted(userId, this._id);
-    Session.set('currentFeatName', this.featName)
-    // console.log(this.featName);
     if (downVoteCheck) {
       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': 1}});
-      Meteor.call('imagesUpsert', this._id, {$pull: {'downVotes': userId}}, function() {
-        setTopVote();
-      });
+      Meteor.call('imagesUpsert', this._id, {$pull: {'downVotes': userId}});
       console.log('removed downvote');
     } else if (!upVoteCheck) {
       console.log('upVoted');
       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': 1}});
-      Meteor.call('imagesUpsert', this._id, {$push: {'upVotes': userId}}, function() {
-        setTopVote();
-      });
+      Meteor.call('imagesUpsert', this._id, {$push: {'upVotes': userId}});
     }
     else {
       console.log('prevented upvote');
@@ -105,23 +111,92 @@ Template.snapshots.events({
 
     var upVoteCheck = hasUpVoted(userId, this._id);
     var downVoteCheck = hasDownVoted(userId, this._id);
-    Session.set('currentFeatName', this.featName)
     if (upVoteCheck) {
       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': -1}});
-      Meteor.call('imagesUpsert', this._id, {$pull: {'upVotes': userId}}, function() {
-        setTopVote();
-      });
+      Meteor.call('imagesUpsert', this._id, {$pull: {'upVotes': userId}});
       console.log('removed downvote');
     } else if (!downVoteCheck) {
       console.log('downvoted');
       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': -1}});
-      Meteor.call('imagesUpsert', this._id, {$push: {'downVotes': userId}}, function() {
-        setTopVote();
-      });
+      Meteor.call('imagesUpsert', this._id, {$push: {'downVotes': userId}});
     } else {
       console.log('prevented upvote');
     }
+  },
+
+  'submit form.new-comment': function(evt, template){
+    evt.preventDefault();
+
+    var input = template.find('.addComment');
+
+    var comment = input.value;
+
+    Meteor.call('imagesUpsert', this._id, {$push: {'comments': {'comment': comment, 'username': this.username}}});
+
+    input.value = '';
+  },
+
+  'submit form.new-title': function(evt, template){
+    evt.preventDefault();
+    var input = template.find('.addTitle');
+
+    var title = input.value;
+
+    Meteor.call('imagesUpsert', this._id, {$set: {'title': title}});
+
+    input.value = '';
   }
 });
+
+
+// Template.snapshots.events({
+//   'click div.upvote': function(evt, template) {
+//     var userId = Meteor.userId();
+//     // console.log(this);
+//     var upVoteCheck = hasUpVoted(userId, this._id);
+//     var downVoteCheck = hasDownVoted(userId, this._id);
+//     Session.set('currentFeatName', this.featName)
+//     // console.log(this.featName);
+//     if (downVoteCheck) {
+//       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': 1}});
+//       Meteor.call('imagesUpsert', this._id, {$pull: {'downVotes': userId}}, function() {
+//         setTopVote();
+//       });
+//       console.log('removed downvote');
+//     } else if (!upVoteCheck) {
+//       console.log('upVoted');
+//       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': 1}});
+//       Meteor.call('imagesUpsert', this._id, {$push: {'upVotes': userId}}, function() {
+//         setTopVote();
+//       });
+//     }
+//     else {
+//       console.log('prevented upvote');
+//     }
+//   },
+
+//   'click div.downvote': function(evt, template) {
+//     var userId = Meteor.userId();
+
+//     var upVoteCheck = hasUpVoted(userId, this._id);
+//     var downVoteCheck = hasDownVoted(userId, this._id);
+//     Session.set('currentFeatName', this.featName)
+//     if (upVoteCheck) {
+//       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': -1}});
+//       Meteor.call('imagesUpsert', this._id, {$pull: {'upVotes': userId}}, function() {
+//         setTopVote();
+//       });
+//       console.log('removed downvote');
+//     } else if (!downVoteCheck) {
+//       console.log('downvoted');
+//       Meteor.call('imagesUpsert', this._id, {$inc: {'voteCount': -1}});
+//       Meteor.call('imagesUpsert', this._id, {$push: {'downVotes': userId}}, function() {
+//         setTopVote();
+//       });
+//     } else {
+//       console.log('prevented upvote');
+//     }
+//   }
+// });
 
 
